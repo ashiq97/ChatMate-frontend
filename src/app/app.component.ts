3afe5fdf9f2105e7fcd8afa35b1e440f24f62f2b
ashiqurrahmankhan <ashiqur.khan@technonext.com>
2025-07-08 18:35:32 +0600

    add login ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { error } from 'console';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -12,18 +14,26 @@ export class AppComponent implements OnInit{
   title = 'The ChatMate App';
   users:any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private accountService: AccountService) {}
 
   ngOnInit(){
     this.getUser();
+    this.setCurrentUser();
   }
-
+  setCurrentUser()
+  {
+    const user:User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);  
+  }
   getUser()
   {
-    this.http.get('http://localhost:36973/api/users').subscribe(response => {
-        this.users = response; 
-        },error => {
+      this.http.get('http://localhost:36973/api/users').subscribe({
+        next:(response)=>{
+           this.users = response; 
+        },
+        error:(error)=>{
           console.log(error);
-        })
+        }
+      })
   }
 } 
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  model:any = {};
+  loogedIn:boolean;
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+  this.accountService.login(this.model).subscribe({
+    next: (response) => {
+      console.log(response);
+    },
+    error: (error) => {
+      console.log(error);
+    }
+  });
+}
+
+  logout()
+  {
+    this.accountService.logout();
+  }
 }
